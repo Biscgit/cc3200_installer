d03f8f7204a6530627588fe877deea3c600c8353
David Horvát <horvatda@proton.me>
2024-07-22 16:44:17 +0200

    fixed issues

--- scripts/templates/install.sh
+++ scripts/templates/install.sh
@@ -4,42 +4,55 @@ echo "Checking for required packages"
 
 # python
 if ! command -v python3 &>/dev/null; then
-  echo "- missing python"
-  sudo apt install -y python3 python3-pip python3-venv
+  REQUIRED_VERSION="3.9"
+  echo "Required Python version: $REQUIRED_VERSION (or higher)"
 
-  echo "+ installed python"
+  sudo apt-get install -y python3
+
+#  echo "Fetching dependencies"
+#  sudo apt-get install -y libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+#
+#  echo "Downloading newer version"
+#  PYTHON_URL="https://www.python.org/ftp/python/3.12.4/Python-3.12.4.tgz"
+#  curl -O $PYTHON_URL
+#
+#  echo "Extracting"
+#  tar -xzf Python-3.12.4.tgz
+#  cd Python-3.12.4 || exit 1
+#
+#  echo "Installing"
+#  ./configure --enable-optimizations
+#  sudo make install
+#
+#  cd ..
+
+  PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+  echo "+ installed python $PYTHON_VERSION"
+
 else
-  echo "+ OK python"
+  echo "+ OK python "
 fi
 
 # rust
 if ! command -v rustc &>/dev/null; then
   echo "- rust"
-
-  # curl
-  if ! command -v curl &>/dev/null; then
-    echo "- curl"
-    sudo apt install -y curl
-    echo "+ installed curl"
-  else
-    echo "+ OK curl"
-  fi
-
-  curl https://sh.rustup.rs -sSf | sh -s -- -y
-  # ToDo extra steps required here
+  sudo apt-get install -y rustc
 
   echo "+ installed rust"
+
 else
   echo "+ OK rust"
 fi
 
-echo "Creating virtual environment"
-python3 -m venv .venv
-source .venv/bin/activate
+# not required -> only one package needed -> pip ssl error
+# echo "Creating virtual environment"
+# python3 -m venv .venv
+# source .venv/bin/activate
 
-echo "Installing packages"
-pip3 install --upgrade pip
-pip3 install asyncssh
+python3 -c 'import asyncssh;' || {
+  echo "Installing missing packages"
+  sudo apt install -y python3-asyncssh
+}
 
 echo "Running script"
 python3 - <<'EOF'
@@ -47,7 +60,7 @@ python3 - <<'EOF'
 EOF
 
 echo "Cleaning up"
-deactivate
-rm -rf .venv
+# deactivate
+# rm -rf .venv
 
 echo "All done!"
